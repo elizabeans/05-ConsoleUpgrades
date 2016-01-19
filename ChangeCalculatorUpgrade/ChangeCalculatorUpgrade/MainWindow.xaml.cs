d6e30989f344f9f2a861c7874838891f1472fb7d
emaraan <emaraan@gmail.com>
2016-01-18 16:08:34 -0800

    added error messages

--- ChangeCalculatorUpgrade/ChangeCalculatorUpgrade/MainWindow.xaml.cs
+++ ChangeCalculatorUpgrade/ChangeCalculatorUpgrade/MainWindow.xaml.cs
@@ -27,28 +27,48 @@ namespace ChangeCalculatorUpgrade
 
         private void buttonCalculate_Click(object sender, RoutedEventArgs e)
         {
-            double cost = Math.Round(double.Parse(textBoxCost.Text), 2);
-            double payment = Math.Round(double.Parse(textBoxPayment.Text), 2);
-            double change = payment - cost;
-            
-            // Show change in cash register window
-            textBlockChange.Text = "$" + string.Format("{0:0.00}", change);
-            
-            // Calculate exact change
-            double[] exact = calculateExactChange(change);
+            try
+            {
+                double cost = Math.Round(double.Parse(textBoxCost.Text), 2);
+                double payment = Math.Round(double.Parse(textBoxPayment.Text), 2);
+                double change = payment - cost;
 
-            // Show exact change to give
-            textBlockExactChange.Text = "You need to give \n" +
-                                        exact[0] + " $100 bill(s) \n" +
-                                        exact[1] + " $50 bill(s) \n" +
-                                        exact[2] + " $20 bill(s) \n" +
-                                        exact[3] + " $10 bill(s) \n" +
-                                        exact[4] + " $5 bill(s) \n" +
-                                        exact[5] + " $1 bill(s) \n" +
-                                        exact[6] + " quarter(s) \n" +
-                                        exact[7] + " dime(s) \n" +
-                                        exact[8] + " nickel(s) and \n" +
-                                        exact[9] + " pennies.";
+                // Show change in cash register window
+                textBlockChange.Text = "$" + string.Format("{0:0.00}", change);
+
+                // Calculate exact change
+                double[] exact = calculateExactChange(change);
+
+                // Show exact change to give
+                textBlockExactChange.Text = "You need to give \n" +
+                                            exact[0] + " $100 bill(s) \n" +
+                                            exact[1] + " $50 bill(s) \n" +
+                                            exact[2] + " $20 bill(s) \n" +
+                                            exact[3] + " $10 bill(s) \n" +
+                                            exact[4] + " $5 bill(s) \n" +
+                                            exact[5] + " $1 bill(s) \n" +
+                                            exact[6] + " quarter(s) \n" +
+                                            exact[7] + " dime(s) \n" +
+                                            exact[8] + " nickel(s) and \n" +
+                                            exact[9] + " pennies.";
+            }
+            catch (FormatException exception)
+            {
+                // Window pops up to display error 
+
+                ErrorMessageWindow window = new ErrorMessageWindow();
+                if (textBoxCost.Text == "Enter cost here" || textBoxPayment.Text == "Enter payment here")
+                {
+                    window.textBlockError.Text = "You forgot to enter the cost and/or payment.";
+                }
+                else
+                {
+                    window.textBlockError.Text = "Please enter a valid number. \n" +
+                                                 "Do not include any currency symbols (i.e. $)";
+                }
+
+                window.Show();
+            }
         }
 
         public double[] calculateExactChange (double change)
